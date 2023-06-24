640d5fd745353585ee6e645ee6c919163bee858d
Theerthani Mallur <theerthanimalluri3009@gmail.com>
2023-06-24 17:51:20 +0530

    completed backend

--- src/App.vue
+++ src/App.vue
@@ -45,19 +45,50 @@ export default {
     data() {
         return {
             image: image,
-            name: "Theerthani Malluri",
-            email: "theerthanimalluri@gmail.com",
-            interests: "coding",
+            name: "",
+            email: "",
+            interests: "",
             isEditMode: false
         }
     },
+    async created() {
+        const userData = await this.fetchUserProfile()
+        this.name = userData.name
+        this.email = userData.email
+        this.interests = userData.interests
+    },
+
     methods: {
         handleEditProfile() {    
             this.isEditMode = true
         },
 
-        handleUpdateProfile() {
+        async handleUpdateProfile() {
+            const payload = {
+                name: this.name,
+                email: this.email,
+                interests: this.interests
+            }
+            const resJson = await this.updateUserProfile(payload)
+            console.log(resJson)
+
             this.isEditMode = false
+        },
+
+        async fetchUserProfile() {
+            const res = await fetch('get-profile')
+            return await res.json()
+        },
+        async updateUserProfile(payload) {
+            const res = await fetch('update-profile', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json', 
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+            return await res.json()
         }
 
     }
